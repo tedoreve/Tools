7945f66f478e4a060ebbe5a5ba394ade78d5be3f
tedoreve <wlzzs666@gmail.com>
2017-03-26 22:42:05 +0800

    auto commit

---
+++ skabc.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 25 21:35:33 2017
+
+@author: tedoreve
+"""
+
+import numpy as np
+import urllib
+from sklearn import preprocessing
+from sklearn import metrics
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import Ridge
+from sklearn.grid_search import GridSearchCV
+# url with dataset
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data"
+# download the file
+raw_data = urllib.request.urlopen(url)
+# load the CSV file as a numpy matrix
+dataset = np.loadtxt(raw_data, delimiter=",")
+# separate the data from the target attributes
+X = dataset[:,0:7]
+y = dataset[:,8]
+
+
+# normalize the data attributes
+normalized_X = preprocessing.normalize(X)
+# standardize the data attributes
+standardized_X = preprocessing.scale(X)
+
+model = ExtraTreesClassifier()
+model.fit(X, y)
+# display the relative importance of each attribute
+print(model.feature_importances_)
+
+model = LogisticRegression()
+model.fit(X, y)
+print(model)
+# make predictions
+expected = y
+predicted = model.predict(X)
+# summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
+
+model = GaussianNB()
+model.fit(X, y)
+print(model)
+# make predictions
+expected = y
+predicted = model.predict(X)
+# summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
+
+# fit a k-nearest neighbor model to the data
+model = KNeighborsClassifier()
+model.fit(X, y)
+print(model)
+# make predictions
+expected = y
+predicted = model.predict(X)
+# summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
+
+# fit a CART model to the data
+model = DecisionTreeClassifier()
+model.fit(X, y)
+print(model)
+# make predictions
+expected = y
+predicted = model.predict(X)
+# summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
+
+# fit a SVM model to the data
+model = SVC()
+model.fit(X, y)
+print(model)
+# make predictions
+expected = y
+predicted = model.predict(X)
+# summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
+
+# prepare a range of alpha values to test
+alphas = np.array([1,0.1,0.01,0.001,0.0001,0])
+# create and fit a ridge regression model, testing each alpha
+model = Ridge()
+grid = GridSearchCV(estimator=model, param_grid=dict(alpha=alphas))
+grid.fit(X, y)
+print(grid)
+# summarize the results of the grid search
+print(grid.best_score_)
+print(grid.best_estimator_.alpha)

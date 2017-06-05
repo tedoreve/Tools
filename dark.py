748737ad596c7f9751f802216a4d94fd6ac96547
tedoreve <wlzzs666@gmail.com>
2017-06-05 16:30:18 +0800

    auto commit

---
+++ dark.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  4 18:00:29 2017
+
+@author: tedoreve
+"""
+
+from astropy import constants as con
+from astropy import units as un
+import numpy as np
+

b943d75f05f02603ea402105a87489a426d1d7b4
tedoreve <wlzzs666@gmail.com>
2017-08-05 19:11:02 +0800

    auto commit

--- worldcloudabc.py
+++ worldcloudabc.py
@@ -11,32 +11,39 @@ Minimal Example
 Generating a square wordcloud from the US constitution using default arguments.
 """
 
-from os import path
 from wordcloud import WordCloud
 import ass
 import codecs 
 import jieba
 
-d = path.dirname(__file__)
-# Read the whole text.
-
-with codecs.open('../data/constitution.ass','r',encoding='utf-16-le') as f:
-    doc = ass.parse(f)
-#text = open(path.join(d, '../data/constitution.ass')).read()
 text =''
-for i in range(len(doc.events)):
-    text += doc.events[i].text
-    
-word_jieba = jieba.cut(text[1000:3000],cut_all=True)  
-word_split = " ".join(word_jieba)  
+
+for i in range(14):
+    with codecs.open('../data/ass/'+ str(i+1) +'.ass','r',encoding='utf-16-le') as f:
+        doc = ass.parse(f)
+        for j in range(len(doc.events)):
+            text += doc.events[j].text 
+#text = open(path.join(d, '../data/constitution.ass')).read()
+
+
+  
+word_jieba = jieba.cut(text,cut_all=False)  
+word_split = ",".join(word_jieba)  
+
+word_list  = word_split.split(',')
+trash      = ['be1','','fad','frz','pos','an7']
+word_list  = list(filter(lambda a: a not in trash, word_list))
+word_split = ",".join(word_list)  
+
 # Generate a word cloud image
-wordcloud = WordCloud(font_path='../data/simsun.ttc').generate(word_split)
+wordcloud  = WordCloud(font_path='../data/simsun.ttc',width=1600, height=800).generate(word_split)
 
 # Display the generated image:
 # the matplotlib way:
 import matplotlib.pyplot as plt
 plt.imshow(wordcloud)
 plt.axis("off")
+#plt.savefig('sample.jpg', dpi = 600)
 
 # lower max_font_size
 #wordcloud = WordCloud(max_font_size=40).generate(text)

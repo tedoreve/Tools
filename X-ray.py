9affec269a8aeaa198867e5cd6a96909db259d2d
tedoreve <wlzzs666@gmail.com>
2017-09-17 21:06:38 +0800

    auto commit

--- X-ray.py
+++ X-ray.py
@@ -5,22 +5,34 @@ Created on Wed Sep  6 22:17:27 2017
 @author: tedoreve
 """
 
-#import numpy as np
-#from astropy import constants as con
+import numpy as np
+from astropy import constants as con
 from astropy import units as un
 
 d = 6*un.kpc
-r1 = 33*un.pc
-r2 = 44*un.pc
-width = (2.36-0.52)*un.keV
-print(width.to('Hz',equivalencies=un.spectral()).value)
+r1 = 9*un.pc
+r2 = 3*un.pc
+V1 = 4/3*np.pi*r1**3
+V2 = 4/3*np.pi*r2**3
+#width = (2.36-0.52)*un.keV
+#print(width.to('Hz',equivalencies=un.spectral()).value)
 
-T = 0.29*un.keV
-lam = 4e-23*un.erg*un.cm**3/un.s
-Fx  = 2e-9*un.erg/un.cm**2/un.s
-c   = 1.2*r2*r2*r1/(r2*r2*2+r2*r1*4)
-n_H = (Fx/c/lam)**0.5
+T = 0.5*un.keV
 print(T.to('K',equivalencies=un.temperature_energy()).value)
+lam1 = 3.5e-23*un.erg*un.cm**3/un.s
+lam2 = 3.4e-23*un.erg*un.cm**3/un.s
+lam3 = 2.5e-23*un.erg*un.cm**3/un.s
+Fx1  = 1.4**2*2.2e35*un.erg/un.s
+Fx2  = 1.4**2*2.7e34*un.erg/un.s
+Fx3  = 1.4**2*1.1e34*un.erg/un.s
+c    = 1.2*(V1-V2)
+n_H  = (Fx2/c/lam2)**0.5
+print(n_H.to('cm^-3').value)
+mh   = con.m_p*n_H*(V1-V2)
+mhe  = 2*con.m_p*n_H*0.1*(V1-V2)
+eh   = con.k_B*T.to('K',equivalencies=un.temperature_energy())*n_H*(V1-V2)
+ehe  = con.k_B*T.to('K',equivalencies=un.temperature_energy())*n_H*0.1*(V1-V2)
+
 #n_e = 0.29*un.cm**-3
 #n_h = n_e/12*10
 #n_he= n_e/12
--- plot_cloudy.py
+++ plot_cloudy.py
@@ -7,3 +7,36 @@ Created on Sat Sep  9 09:59:06 2017
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from astropy import units as un
+from astropy import constants as con
+
+
+def cooling():
+    dex  = np.linspace(1,17,17)
+    def f(x):
+        return 'str' + x
+    names = map(f, dex.astype(int).astype(str))
+    data = pd.read_table('D:\software\c17.00\source\script.dat',names=names,encoding='gb2312')
+
+    plt.plot(data['str1'],data['str5'])
+    plt.plot([17406778.95421341,]*17,dex*1e-23)
+    plt.plot(dex*1e6,[2.5e-23,]*17)
+    ##plt.xlim(0.52,2.36)
+    ##plt.ylim(1e36,1e39)
+    plt.xscale('log')
+    plt.yscale('log')
+    
+def sed():
+    data = pd.read_table('D:\software\c17.00\source\script.con',encoding='gb2312')
+    plt.plot(data['#Cont  nu'],data['total'])
+    #e    = (data['nu'].values*con.Ryd).to('keV',equivalencies=un.spectral())
+#    plt.ylim(1e-6,1e-2)
+    plt.xscale('log')
+    plt.yscale('log')
+    
+#==============================================================================
+if __name__ == '__main__':
+    cooling()
+#    sed()
+#==============================================================================

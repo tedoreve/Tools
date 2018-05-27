0b58fc7f606566b898b7871ac0894607fe21216a
tedoreve <wlzzs666@gmail.com>
2018-05-27 10:37:06 +0800

    auto commit

--- naimaabc/naimaabc.py
+++ naimaabc/naimaabc.py
@@ -1,62 +1,53 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May  4 22:02:26 2018
-
-@author: tedoreve
-"""
-
 import numpy as np
+import matplotlib.pyplot as plt
+
 import naima
+from naima.models import (ExponentialCutoffPowerLaw, Synchrotron,
+                          InverseCompton)
+from astropy.constants import c
 import astropy.units as u
-from astropy.io import ascii
-from naima.models import InverseCompton, ExponentialCutoffPowerLaw
 
-## Read data
+ECPL = ExponentialCutoffPowerLaw(1e36*u.Unit('1/eV'), 1*u.TeV, 2.1, 13*u.TeV)
+SYN = Synchrotron(ECPL, B=100*u.uG)
 
-data = ascii.read('RXJ1713_HESS_2007.dat')
+# Define energy array for synchrotron seed photon field and compute
+# Synchroton luminosity by setting distance to 0.
+Esy = np.logspace(-6, 6, 100)*u.eV
+Lsy = SYN.flux(Esy, distance=0*u.cm)
+
+# Define source radius and compute photon density
+R = 2 * u.pc
+phn_sy = Lsy / (4 * np.pi * R**2 * c) * 2.26
+
+# Create IC instance with CMB and synchrotron seed photon fields:
+IC = InverseCompton(ECPL, seed_photon_fields=['CMB', 'FIR', 'NIR',
+                                              ['SSC', Esy, phn_sy]])
+
+# Compute SEDs
+spectrum_energy = np.logspace(-1,14,100)*u.eV
+sed_IC = IC.sed(spectrum_energy, distance=1.5*u.kpc)
+sed_SYN = SYN.sed(spectrum_energy, distance=1.5*u.kpc)
+
+# Plot
+plt.figure(figsize=(8,5))
+#plt.rc('font', family='sans')
+#plt.rc('mathtext', fontset='custom')
+ssc = IC.sed(spectrum_energy, seed='SSC', distance=1.5*u.kpc)
+plt.loglog(spectrum_energy,ssc,lw=1.5,
+        ls='-',label='IC (SSC)',c=naima.plot.color_cycle[2])
+for seed, ls in zip(['CMB','FIR','NIR'], ['-','--',':']):
+    sed = IC.sed(spectrum_energy, seed=seed, distance=1.5*u.kpc)
+    plt.loglog(spectrum_energy,sed,lw=1,
+            ls=ls,c='0.25')#,label='IC ({0})'.format(seed))
 
 
-def ElectronIC(pars, data):
-    """
-    Define particle distribution model, radiative model, and return model flux
-    at data energy values
-    """
-
-    ECPL = ExponentialCutoffPowerLaw(pars[0] / u.eV, 10. * u.TeV, pars[1],
-                                     10**pars[2] * u.TeV)
-    IC = InverseCompton(ECPL, seed_photon_fields=['CMB'])
-
-    return IC.flux(data, distance=1.0 * u.kpc)
-
-
-def lnprior(pars):
-    # Limit amplitude to positive domain
-    logprob = naima.uniform_prior(pars[0], 0., np.inf)
-    return logprob
-
-
-if __name__ == '__main__':
-
-    ## Set initial parameters and labels
-    p0 = np.array((1e30, 3.0, np.log10(30),))
-    labels = ['norm', 'index', 'log10(cutoff)']
-
-    ## Run sampler
-    sampler, pos = naima.run_sampler(data_table=data,
-                                     p0=p0,
-                                     labels=labels,
-                                     model=ElectronIC,
-                                     prior=lnprior,
-                                     nwalkers=32,
-                                     nburn=100,
-                                     nrun=20,
-                                     threads=4,
-                                     prefit=True,
-                                     interactive=False)
-    ## Save run results
-    out_root = 'RXJ1713_IC_minimal'
-    naima.save_run(out_root, sampler)
-
-    ## Save diagnostic plots and results table
-    naima.save_diagnostic_plots(out_root, sampler, sed=True)
-    naima.save_results_table(out_root, sampler)
+plt.loglog(spectrum_energy,sed_IC,lw=2,
+        label='IC (total)',c=naima.plot.color_cycle[0])
+plt.loglog(spectrum_energy,sed_SYN,lw=2,label='Sync',c=naima.plot.color_cycle[1])
+plt.xlabel('Photon energy [{0}]'.format(
+        spectrum_energy.unit.to_string('latex_inline')))
+plt.ylabel('$E^2 dN/dE$ [{0}]'.format(
+        sed_SYN.unit.to_string('latex_inline')))
+plt.ylim(1e-12, 1e-6)
+plt.tight_layout()
+plt.legend(loc='lower left')

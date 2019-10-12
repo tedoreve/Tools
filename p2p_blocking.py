104b02f8a1ffd53d6c569eb9c16e9d37499746f1
tedoreve <wlzzs666@gmail.com>
2019-10-12 17:35:19 +0800

    new

--- p2p_blocking.py
+++ p2p_blocking.py
@@ -1,13 +1,45 @@
+# send_recv_timing.pu
+
+import time
+import numpy as np
 from mpi4py import MPI
 
+def f(x, y):
+    return x+y, y
+
+
+flux_vol = np.zeros([4,4,4,2])
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size()
+
+per = flux_vol.shape[2]//size
+
+print(per)
+print(flux_vol.shape[0])
+
+comm.Barrier()
+start_time = time.time()
+
+for i in range(per * rank, per * (rank+1)):
+    for j in range(flux_vol.shape[1]):
+        for k in range(flux_vol.shape[0]):
+            # for l in range(2):
+                flux_vol[i,j,k,:] = f(i*j*k, i-j-k)
+
 
 if rank == 0:
-    data = {'a': 7, 'b': 3.14}
-    print(rank, data)
-    comm.send(data, dest=1, tag=11)
-elif rank == 1:
-    data = comm.recv(source=0, tag=11)
-    print(rank, data)
+    total = np.zeros([flux_vol.shape[2],flux_vol.shape[1],flux_vol.shape[0],2])
+else:
+    total = None
+
+comm.Barrier()
+#collect the partial results and add to the total sum
+comm.Reduce(flux_vol, total, op=MPI.SUM, root=0)
+
+stop_time = time.time()
+
+if rank == 0:
+    print ("-----", (stop_time-start_time)*1000, "-----")
+    total.tofile("a.bin")

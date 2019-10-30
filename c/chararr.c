896bd5b08c25b0a3fdfcab864b8895894e0ecb69
tedoreve <wlzzs666@gmail.com>
2019-10-30 13:39:20 +0800

    c

--- c/chararr.c
+++ c/chararr.c
@@ -1,4 +1,8 @@
-#include <stdio.h>
+
+#include "math.h"
+#include "time.h"
+#include "stdlib.h"
+#include "stdio.h"
 
 #define MAXLINE 1000 /* maximum input line length */
 // int getline(char line[], int maxline);
@@ -6,42 +10,8 @@
 
 main()
 {
-	int len; /* current line length */
-	int max; /* maximum length seen so far */
-	char line[MAXLINE]; /* current input line */
-	char longest[MAXLINE]; /* longest line saved here */
-	
-	max = 0;
-	while ((len = getlines(line, MAXLINE)) > 0){
-		if (len > max) {
-			max = len;
-			copys(longest, line);
-		}
-	}
-	if (max > 0) /* there was a line */
-		printf("%s", longest);
+	srand(1000000000);
+	double a = rand() % 20 - 10;
+	printf("%f \n", a);
 	return 0;
-}
-/* getline: read a line into s, return length */
-getlines(char s[],int lim)
-{
-	int c, i;
-	
-	for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-		s[i] = c;
-	if (c == '\n') {
-		s[i] = c;
-		++i;
-	}
-	s[i] = '\0';
-	return i;
-}
-/* copy: copy 'from' into 'to'; assume to is big enough */
-copys(char to[], char from[])
-{
-	int i;
-	
-	i = 0;
-	while ((to[i] = from[i]) != '\0')
-		++i;
 }
